b1b9aa47771aadc66f2ba9ea442b5dc734f94e50
linghtiin <wrqal666@gmail.com>
2017-05-19 02:33:07 +0800

    快速排序

--- course/T1/mai7 sort_bubble.cpp
+++ course/T1/mai7 sort_bubble.cpp
@@ -12,25 +12,52 @@ typedef struct LIST
 
 void bubble_sort(List l[],int n)
 {
-    int i,j,f=1;
+    int i=1,j,f=1;
     List t;
-    for(i=0;i<n;i++)
-        while(i<n&&f=1)
-        {
-            f=0;
-            for(j=0;j<n-i;j++)
+    while(i<n&&f==1)
+    {
+        f=0;
+        for(j=1;j<=n-i;j++)
+            if(l[j]->key>l[j+1]->key)
             {
-                if(l[j]->key>l[j+1]->key)
-                {
-                    t=l[j];
-                    l[j]=l[j+1];
-                    l[j+1]=t;
-                    f=1;
-                }
+                t=l[j];
+                l[j]=l[j+1];
+                l[j+1]=t;
+                f=1;
             }
-        }
+    i++;
+    }
 
         
+}
+
+void Qsort(List l[],int low.int high)
+{
+    int m;
+    if(low<high){
+        m=part(l,low,high);
+        Qsort(l,low,m-1);
+        Qsort(l,m+1,high);
+        
+    }
+}
+
+void part(List l[],int i,int j)
+{
+    int key;
+    l[0]=l[i];
+    key=l[0]->key;
+    while(i<j){
+        while(i<j&&key>=l[j]->key)
+            j--;
+        l[i]=l[j];
+        while(i<j&&key<l[i]->key)
+            i++;
+        l[j]=l[i];
+        
+    }
+    l[i]=l[0];
+    return i;
 }
 
 int main()
