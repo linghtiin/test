048550acc169c63526b48ab1ef39be3540c0c825
linghtiin <wrqal666@gmail.com>
2017-05-20 18:06:34 +0800

    额外，字符串处理。

--- course/exim_1.cpp
+++ course/exim_1.cpp
@@ -1,23 +1,108 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 #define N 1000
 
+typedef struct my_queue
+{
+    char v[N];
+    int f;
+    int r;
+}QUE;
+
+void init(QUE *q);
+void add_queue(QUE *q,char x);
+char del_queue(QUE *q);
+
+
 int main ()
 {
-	char n[N],m[N],o[N],p[N]={"\0"};
-	int i,j=0;
-	
+	char n[N],m[N],o[N],*p,*p1;
+	QUE *q,q1;
+
+	int i,j,k,l,l_m;
+
+
+	q=&q1;
+
+	init(q);
+
 	scanf("%s",n);
 	scanf("%s",m);
 	scanf("%s",o);
-	
-	for(i=0;i<N&&n[i]!='\0';i++)
-	    for(j=0;j<N&&m[j]!='\0'&&n[i+j]==m[j];j++)
-	      if(m[j]=='\0')
-	        for()
-	
-	
-	printf("%s",p);
-	
+
+	l=strlen(n);
+	l_m=strlen(m);
+	p=n+1;
+
+	for(i=0;i<=l;i++)
+    {
+        p1=strstr(p-1,m);
+        if(p1!=NULL)
+        {
+            k=p1-p;
+            printf("%d\n",k);
+            if(k==-1&&p!=n+1)
+                q->r=q->r-1;
+            for(j=0;j<k;j++)
+                add_queue(q,p[j]);
+            for(j=0;o[j]!='\0';j++)
+                add_queue(q,o[j]);
+            p=p1+l_m;
+        }
+        else
+        {
+            for(j=0;p[j]!='\0';j++)
+                add_queue(q,p[j]);
+            break;
+        }
+    }
+
+
+
+
+    while(q->f!=q->r)
+        printf("%c",del_queue(q));
+    printf("\n");
+
 	return 0;
+}
+
+
+
+void init(QUE *q)
+{
+    q->f=-1;
+    q->r=-1;
+}
+
+void add_queue(QUE *q,char x)
+{
+    if((q->r+1)%N==q->f)
+    {
+        printf("the queue is full.\n");
+        exit(1);
+    }
+    else
+    {
+        q->r=(q->r+1)%N;
+        q->v[q->r]=x;
+    }
+}
+
+char del_queue(QUE *q)
+{
+    char x;
+    if(q->f==q->r)
+    {
+        printf("the queue is empty.\n");
+        exit(2);
+    }
+    else
+    {
+        q->f=(q->f+1)%N;
+        x=q->v[q->f];
+    }
+    return x;
 }
--- course/exim_2.cpp
+++ course/exim_2.cpp
@@ -6,12 +6,12 @@ int main ()
 {
 	int n;
 	int i,j;
-	
+
 	scanf("music-%d.MP3",&n);
 	scanf("%d",&i);
-	
+
 	for(j=1;i>0;i--,j++)
-	    printf("music-%d.MP3",&j);
-	
+	    printf("music-%d.MP3\n",j+n);
+
 	return 0;
 }
