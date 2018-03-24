d358c57c3124a02cdba8c104696b9df230cb920d
linghtiin <wrqal666@gmail.com>
2018-03-25 00:29:54 +0800

    文件移动

---
+++ course/T1/exim_2.cpp
@@ -0,0 +1,24 @@
+/*
+
+    输入music-?.MP3与n;
+    输出music-?+1.MP3至music-?+n.MP3的字符串.
+
+*/
+
+#include<stdio.h>
+
+#define N 1000
+
+int main ()
+{
+	int n;
+	int i,j;
+
+	scanf("music-%d.MP3",&n);
+	scanf("%d",&i);
+
+	for(j=1;i>0;i--,j++)
+	    printf("music-%d.MP3\n",j+n);
+
+	return 0;
+}
