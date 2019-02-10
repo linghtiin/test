442762e870a2f15a293ed40883b13e39b5f73955
linghtiin <wrqal666@gmail.com>
2019-02-11 01:06:06 +0800

    完成威士忌分类。

--- Python/Courese/whiskies/class_whiskies.py
+++ Python/Courese/whiskies/class_whiskies.py
@@ -7,11 +7,39 @@ Created on Wed Jan 16 01:59:45 2019
 import os
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster.bicluster import SpectralCoclustering
 
 workpath = os.getcwd()
 whiskies = pd.read_csv(workpath + '\\' + 'whiskies.txt')
 whiskies['Region'] = pd.read_csv(workpath + '\\' + 'regions.txt')
+whiskies_data = whiskies.iloc[:,2:14]
 
 #相关性矩阵
-corr_whiskies = pd.DataFrame.corr(whiskies)
+corr_whiskies = pd.DataFrame.corr(whiskies_data.transpose())
 
+#聚类，重新排序
+model = SpectralCoclustering(n_clusters=6,random_state=0)
+model.fit(corr_whiskies)
+np.sum(model.rows_,axis=1)
+
+whiskies['Group'] = pd.Series(model.row_labels_,index=whiskies.index)
+whiskies = whiskies.iloc[np.argsort(model.row_labels_)]
+whiskies = whiskies.reset_index(drop=True)
+
+#聚类排序后的相关性矩阵
+correlations = pd.DataFrame.corr(whiskies.iloc[:,2:14].transpose())
+correlations = np.array(correlations)
+
+#绘图
+plt.figure(figsize=(20,10))
+plt.subplot(121)
+plt.pcolor(corr_whiskies)
+plt.title("Original")
+plt.axis('tight')
+plt.subplot(122)
+plt.pcolor(correlations)
+plt.title("Rearranged")
+plt.axis('tight')
+plt.savefig("correlations.pdf")
+plt.show()
